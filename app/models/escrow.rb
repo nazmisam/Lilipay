8ce66dcecfd88deca0eb2eeb6ea2b28bc19cce68
Nazmi Sam <m.nazmi124@gmail.com>
2022-08-15 17:31:16 +0800

    updated payment

--- app/models/escrow.rb
+++ app/models/escrow.rb
@@ -16,7 +16,7 @@ class Escrow < ApplicationRecord
     end
     
     def generate_checksum
-        data = "#{buyer_email}|#{buyer_name}|#{shipping_attention}|#{""}|#{transaction_number}|#{description}|http://localhost:3000/users/escrows/paymentredirect|#{total_pay}|#{"02b66d73-c60f-47e6-a07c-0aa3609ddddd"}"
+        data = "#{buyer_email}|#{buyer_name}|#{shipping_attention}|#{""}|#{transaction_number}|#{description}|http://#{ENV["domain"]}/users/escrows/paymentredirect|#{total_pay}|#{"02b66d73-c60f-47e6-a07c-0aa3609ddddd"}"
         puts(data)
         checksum_token = "a69b33918639440dbc7adc2f0fea60f67d7d675d3a2876f061832eecd4c04592"
         OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), checksum_token, data)
