c36a7285bc40e27ff44ca767ff35424064495183
Nazmi Sam <m.nazmi124@gmail.com>
2022-08-19 15:52:02 +0800

    changed sessions/registrations to slim

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :branch, 'main'
 # Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
 # run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
 set :shared_dirs, fetch(:shared_dirs, []).push('public/assets', 'storage', 'node_modules')
-set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/master.key', 'config/application.yml')
+set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/master.key')
 
 # This task is the environment that is loaded for all remote run commands, such as
 # `mina deploy` or `mina rake`.
